e68a4254fd58b9900bf4381031f70da0c89362f4
mazbrili <mazbrili@yahoo.com>
2022-11-12 11:44:04 +0700

    Catatan NLP

---
+++ NLP.mm
@@ -0,0 +1,97 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="1.0.1">
+<!-- This file is saved using a hacked version of FreeMind. visit: http://freemind-mmx.sourceforge.net -->
+<!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
+<!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
+<node ID="ID_1755176027" 
+	TEXT="NLP">
+<icon BUILTIN="gohome"/>
+<node FOLDED="true" ID="ID_1768259509" POSITION="right" 
+	TEXT="Menghilangkan mental block">
+<node FOLDED="true" ID="ID_645060312" 
+	TEXT="mental block -&gt; keyakinan negatif yang tertanam">
+<node ID="ID_862784262" 
+	TEXT="Limiting believe"/>
+<node FOLDED="true" ID="ID_608328502" 
+	TEXT="limiting decision">
+<node FOLDED="true" ID="ID_1746122591" 
+	TEXT="mengorek pengalaman dulu">
+<node FOLDED="true" ID="ID_19432694" 
+	TEXT="korek - maksud baik dibalik perkataan">
+<node ID="ID_1487349560" 
+	TEXT="misal:kamu bodo -&gt; maksudnya biar belajar"/>
+</node>
+</node>
+<node FOLDED="true" ID="ID_1457231770" 
+	TEXT="diminta untuk menuliskan sebagai orang ketiga">
+<node ID="ID_1003705635" 
+	TEXT="bisa sebagai diri sendiri menasehati dia sendiri"/>
+<node FOLDED="true" ID="ID_1932868865" 
+	TEXT="cari kemenangan2 di masa lalu">
+<node ID="ID_1572482439" 
+	TEXT="ambil pelajaran -&gt; ulang kemengangan"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node FOLDED="true" ID="ID_1473772378" POSITION="left" 
+	TEXT="Bangkit dari kegagalan">
+<icon BUILTIN="stop-sign"/>
+<node FOLDED="true" ID="ID_803268364" 
+	TEXT="re-framing ">
+<icon BUILTIN="password"/>
+<node ID="ID_1921356161" 
+	TEXT="mencari tahu apa maksud baik"/>
+</node>
+<node ID="ID_1498116746" 
+	TEXT="mengambil pelajarn">
+<icon BUILTIN="attach"/>
+</node>
+<node FOLDED="true" ID="ID_572177743" 
+	TEXT="cari mentor">
+<icon BUILTIN="female1"/>
+<node FOLDED="true" ID="ID_523939512" 
+	TEXT="bisa pakai mental mentor">
+<node ID="ID_1216673056" 
+	TEXT="membayangkan orang lain menasehati (3rd view)"/>
+</node>
+</node>
+<node ID="ID_425514512" 
+	TEXT="anda berjuang untuk siapa">
+<icon BUILTIN="help"/>
+</node>
+<node ID="ID_1729537646" 
+	TEXT="temukan strategi baru">
+<icon BUILTIN="kmail"/>
+</node>
+</node>
+<node FOLDED="true" ID="ID_1291820865" POSITION="left" 
+	TEXT="Jangan berkata susah/tidak bisa">
+<icon BUILTIN="stop-sign"/>
+<node FOLDED="true" ID="ID_1327123843" 
+	TEXT="penciptaan terjadi 2x">
+<icon BUILTIN="clanbomber"/>
+<node FOLDED="true" ID="ID_1306076246" 
+	TEXT="terjadi dalam pemikiran">
+<node ID="ID_185719757" 
+	TEXT="tentukan tujuan"/>
+</node>
+<node ID="ID_796452553" 
+	TEXT="terjadi karena aksi"/>
+</node>
+<node FOLDED="true" ID="ID_1225235688" 
+	TEXT="kalau kita berkata tidak bisa/tidak mampu">
+<node ID="ID_1165294191" 
+	TEXT="Menjadikan otak tidak mau memproses"/>
+<node ID="ID_1475457455" 
+	TEXT="maka otak akan mem-blok"/>
+</node>
+<node FOLDED="true" ID="ID_48829021" 
+	TEXT="Rangsang pikiran untuk membuat ide">
+<node ID="ID_1929223379" 
+	TEXT="katakan-saya cari ide dulu"/>
+</node>
+</node>
+</node>
+</map>
